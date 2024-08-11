b6b51467fc3f9f22dc86b1b1a41e9f983dd62bf3
Anthony Mendez <anthonymendez9@gmail.com>
2024-08-11 13:11:35 -0700

    Change how SimplePickaxeTool calculates tooltip damage and speed.

--- common/src/main/java/com/anthonymendez/items/SimplePickaxeTool.java
+++ common/src/main/java/com/anthonymendez/items/SimplePickaxeTool.java
@@ -1,17 +1,15 @@
 package com.anthonymendez.items;
 
+import com.mojang.datafixers.util.Pair;
 import java.util.List;
-import net.minecraft.component.type.AttributeModifiersComponent;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 
 /** Extension of {@link PickaxeItem} to allow for more customization and behaviour in the future. */
-public class SimplePickaxeTool extends PickaxeItem {
+public class SimplePickaxeTool extends PickaxeItem implements ISimpleTool {
   private final float attackDamage;
   private final float attackSpeed;
 
@@ -21,25 +19,10 @@ public class SimplePickaxeTool extends PickaxeItem {
    */
   public SimplePickaxeTool(ToolMaterial material) {
     super(material, new Settings());
-
-    List<AttributeModifiersComponent.Entry> modifiers =
-        createAttributeModifiers(material, 2.0F, 1.2F).modifiers();
-    this.attackDamage =
-        (float)
-            modifiers.stream()
-                .map(AttributeModifiersComponent.Entry::modifier)
-                .filter(modifier -> modifier.idMatches(BASE_ATTACK_DAMAGE_MODIFIER_ID))
-                .findFirst()
-                .orElseThrow()
-                .value();
-    this.attackSpeed =
-        (float)
-            modifiers.stream()
-                .map(AttributeModifiersComponent.Entry::modifier)
-                .filter(modifier -> modifier.idMatches(BASE_ATTACK_SPEED_MODIFIER_ID))
-                .findFirst()
-                .orElseThrow()
-                .value();
+    Pair<Float, Float> attackDamageAndSpeed =
+        SimpleToolUtils.getAttackDamageAndSpeedFromMiningToolItem(this, 2.0F, 1.2F);
+    this.attackDamage = attackDamageAndSpeed.getFirst();
+    this.attackSpeed = attackDamageAndSpeed.getSecond();
   }
 
   /** Returns this current instance as an {@link ItemStack}. */
@@ -47,18 +30,18 @@ public class SimplePickaxeTool extends PickaxeItem {
     return new ItemStack(this);
   }
 
-  /** */
+  /**
+   * Appends damage and speed to the {@link net.minecraft.item.Item.TooltipContext} of the item.
+   * Stats are shown based on default Minecraft standards.
+   *
+   * <p>TODO(Anthony Mendez): Figure out if we can just pull the default Minecraft mining tool item
+   * tooltip.
+   */
   @Override
   public void appendTooltip(
       ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
     super.appendTooltip(stack, context, tooltip, type);
-    tooltip.add(Text.of(""));
-    tooltip.add(Text.translatable("When in Main Hand:").formatted(Formatting.GRAY));
-    tooltip.add(
-        Text.translatable(String.format(" %.1f Attack Damage", this.attackDamage))
-            .formatted(Formatting.DARK_GREEN));
-    tooltip.add(
-        Text.translatable(String.format(" %.1f Attack Speed", this.attackSpeed))
-            .formatted(Formatting.DARK_GREEN));
+    SimpleToolUtils.AppendDefaultMinecraftMiningItemTooltip(
+        tooltip, this.attackDamage, this.attackSpeed);
   }
 }
