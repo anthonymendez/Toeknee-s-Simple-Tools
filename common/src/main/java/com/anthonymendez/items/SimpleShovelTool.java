03a08c9db8319383cf1a33a56caebb86aeb43e1c
Anthony Mendez <anthonymendez9@gmail.com>
2024-08-12 22:40:27 -0700

    Fixed shovel damage and refactored base stats variables

--- common/src/main/java/com/anthonymendez/items/SimpleShovelTool.java
+++ common/src/main/java/com/anthonymendez/items/SimpleShovelTool.java
@@ -2,6 +2,9 @@ package com.anthonymendez.items;
 
 import com.mojang.datafixers.util.Pair;
 import java.util.List;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShovelItem;
 import net.minecraft.item.ToolMaterial;
@@ -10,6 +13,9 @@ import net.minecraft.text.Text;
 
 /** Extension of {@link ShovelItem} to allow for more customization and behaviour in the future. */
 public class SimpleShovelTool extends ShovelItem implements ISimpleTool {
+  public static final float BASE_ATTACK_DAMAGE = 2.0F;
+  public static final float BASE_ATTACK_SPEED = 1.0F;
+
   private final float attackDamage;
   private final float attackSpeed;
 
@@ -20,7 +26,8 @@ public class SimpleShovelTool extends ShovelItem implements ISimpleTool {
   public SimpleShovelTool(ToolMaterial material) {
     super(material, new Settings());
     Pair<Float, Float> attackDamageAndSpeed =
-        SimpleToolUtils.getAttackDamageAndSpeedFromMiningToolItem(this, 2.0F, 1.0F);
+        SimpleToolUtils.getAttackDamageAndSpeedFromShovelItem(
+            this, BASE_ATTACK_DAMAGE, BASE_ATTACK_SPEED);
     this.attackDamage = attackDamageAndSpeed.getFirst();
     this.attackSpeed = attackDamageAndSpeed.getSecond();
   }
@@ -42,5 +49,12 @@ public class SimpleShovelTool extends ShovelItem implements ISimpleTool {
       ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
     super.appendTooltip(stack, context, tooltip, type);
     SimpleToolUtils.AppendDefaultMinecraftMiningItemTooltip(tooltip, attackDamage, attackSpeed);
+  }
+
+  /** Overrides attack damage and returns the base damage calculated in the constructor. */
+  @Override
+  public float getBonusAttackDamage(
+      Entity target, float baseAttackDamage, DamageSource damageSource) {
+    return attackDamage;
   }
 }
