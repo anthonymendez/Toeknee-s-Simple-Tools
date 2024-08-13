d5251bb1eeaf90223f8525f0a758f9e85e70540f
Anthony Mendez <anthonymendez9@gmail.com>
2024-08-12 22:39:21 -0700

    Adjusted Axe Tool Damage and Speed printout

--- common/src/main/java/com/anthonymendez/items/SimpleAxeTool.java
+++ common/src/main/java/com/anthonymendez/items/SimpleAxeTool.java
@@ -1,29 +1,40 @@
 package com.anthonymendez.items;
 
 import com.mojang.datafixers.util.Pair;
-import net.minecraft.item.AxeItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ToolMaterial;
+import java.util.List;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.item.*;
 import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.text.Text;
 
-import java.util.List;
-
 /** Extension of {@link AxeItem} to allow for more customization and behaviour in the future. */
 public class SimpleAxeTool extends AxeItem implements ISimpleTool {
+  public static final float BASE_ATTACK_DAMAGE = 7.0F;
+  public static final float BASE_ATTACK_SPEED = 0.8F;
+
   private final float attackDamage;
   private final float attackSpeed;
 
   /**
    * Creates new {@link SimpleAxeTool} with the given {@link ToolMaterial}. A default {@link
-   * net.minecraft.item.Item.Settings} is used.
+   * Settings} is used.
    */
   public SimpleAxeTool(ToolMaterial material) {
     super(material, new Settings());
     Pair<Float, Float> attackDamageAndSpeed =
-        SimpleToolUtils.getAttackDamageAndSpeedFromMiningToolItem(this, 7.0F, 0.8F);
-    this.attackDamage = attackDamageAndSpeed.getFirst();
-    this.attackSpeed = attackDamageAndSpeed.getSecond();
+        SimpleToolUtils.getAttackDamageAndSpeedFromAxeItem(
+            this, BASE_ATTACK_DAMAGE, BASE_ATTACK_SPEED);
+    // Manually setting attack damage to match other axes.
+    this.attackDamage =
+        (switch (material) {
+          case SimpleToolMaterials.COPPER,
+                  SimpleToolMaterials.EMERALD,
+                  SimpleToolMaterials.QUARTZ ->
+              9.0F;
+          default -> BASE_ATTACK_DAMAGE;
+        });
+    this.attackSpeed = attackDamageAndSpeed.getSecond() + getBonusAttackSpeed();
   }
 
   /** Returns this current instance as an {@link ItemStack}. */
@@ -33,8 +44,8 @@ public class SimpleAxeTool extends AxeItem implements ISimpleTool {
   }
 
   /**
-   * Appends damage and speed to the {@link net.minecraft.item.Item.TooltipContext} of the item.
-   * Stats are shown based on default Minecraft standards.
+   * Appends damage and speed to the {@link TooltipContext} of the item. Stats are shown based on
+   * default Minecraft standards.
    *
    * <p>TODO(Anthony Mendez): Figure out if we can just pull the default Minecraft mining tool item
    * tooltip.
@@ -44,5 +55,25 @@ public class SimpleAxeTool extends AxeItem implements ISimpleTool {
       ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
     super.appendTooltip(stack, context, tooltip, type);
     SimpleToolUtils.AppendDefaultMinecraftMiningItemTooltip(tooltip, attackDamage, attackSpeed);
+  }
+
+  /**
+   * Returns the equivalent vanilla Axe damage for the tool.
+   *
+   * <p>Axe damage seems to be done manually so we won't mess with it.
+   */
+  @Override
+  public float getBonusAttackDamage(
+      Entity target, float baseAttackDamage, DamageSource damageSource) {
+    return attackDamage;
+  }
+
+  /** Returns the bonus attack speed based on the {@link SimpleToolMaterials}. */
+  public float getBonusAttackSpeed() {
+    return switch (getMaterial()) {
+      case SimpleToolMaterials.EMERALD, SimpleToolMaterials.QUARTZ -> 0.2F;
+      case SimpleToolMaterials.COPPER -> 0.1F;
+      default -> 0.0F;
+    };
   }
 }
